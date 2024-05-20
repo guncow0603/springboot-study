a0707ea605ec9daf6cb04887bca0250fee8f2b8c
kimgunwoo <guncow0603@gmail.com>
2024-05-20 21:23:18 +0900

    조회 서비스 코드 작성

--- src/main/java/me/kimgunwoo/springbootstudy/service/BlogService.java
+++ src/main/java/me/kimgunwoo/springbootstudy/service/BlogService.java
@@ -6,6 +6,8 @@ import me.kimgunwoo.springbootstudy.dto.AddArticleRequest;
 import me.kimgunwoo.springbootstudy.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @Not Null이 붙은 필드의 생성자 추가
 @Service // 빈으로 둥록
 public class BlogService {
@@ -15,5 +17,9 @@ public class BlogService {
     //블로그 글 추가 메서드
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
