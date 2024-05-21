fc3999ce5c37dbb97b75cdfaaf2edb7436ef5d7c
kimgunwoo <guncow0603@gmail.com>
2024-05-21 16:36:19 +0900

    BlogViewController 작성

---
+++ src/main/java/me/kimgunwoo/springbootstudy/controller/BlogViewController.java
@@ -0,0 +1,28 @@
+package me.kimgunwoo.springbootstudy.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.kimgunwoo.springbootstudy.dto.ArticleListViewResponse;
+import me.kimgunwoo.springbootstudy.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
