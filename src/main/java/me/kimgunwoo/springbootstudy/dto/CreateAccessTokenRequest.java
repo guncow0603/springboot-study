519785858a1dd3285afc6b6bcae740d0bc29ad42
kimgunwoo <guncow0603@gmail.com>
2024-05-24 16:38:29 +0900

    CreateAccessTokenRequest 작성

---
+++ src/main/java/me/kimgunwoo/springbootstudy/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.kimgunwoo.springbootstudy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
