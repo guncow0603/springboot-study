04f12ccbb94f17fa7a5cdb2e978ede2f022fe214
kimgunwoo <guncow0603@gmail.com>
2024-05-24 16:37:02 +0900

    TokenApiController 작성

---
+++ src/main/java/me/kimgunwoo/springbootstudy/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package me.kimgunwoo.springbootstudy.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.kimgunwoo.springbootstudy.dto.CreateAccessTokenRequest;
+import me.kimgunwoo.springbootstudy.dto.CreateAccessTokenResponse;
+import me.kimgunwoo.springbootstudy.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
