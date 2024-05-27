0f0fa2cb3c361a694749522d241ea7bf2934a205
kimgunwoo <guncow0603@gmail.com>
2024-05-27 17:20:11 +0900

    BlogService 수정

--- src/main/java/me/kimgunwoo/springbootstudy/service/BlogService.java
+++ src/main/java/me/kimgunwoo/springbootstudy/service/BlogService.java
@@ -1,44 +1,60 @@
 package me.kimgunwoo.springbootstudy.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.kimgunwoo.springbootstudy.domain.Article;
 import me.kimgunwoo.springbootstudy.dto.AddArticleRequest;
 import me.kimgunwoo.springbootstudy.dto.UpdateArticleRequest;
 import me.kimgunwoo.springbootstudy.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@RequiredArgsConstructor // final이 붙거나 @Not Null이 붙은 필드의 생성자 추가
-@Service // 빈으로 둥록
+@RequiredArgsConstructor
+@Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    //블로그 글 추가 메서드
-    public Article save(AddArticleRequest request){
-        return blogRepository.save(request.toEntity());
+    public Article save(AddArticleRequest request, String userName) {
+        return blogRepository.save(request.toEntity(userName));
     }
 
-    public List<Article> findAll(){
+    public List<Article> findAll() {
         return blogRepository.findAll();
     }
 
-    public Article findById(long id){
-        return blogRepository.findById(id).orElseThrow(()->new IllegalArgumentException("not found: " + id));
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
     }
 
-    public void delete(long id){
-        blogRepository.deleteById(id);
+    public void delete(long id) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
-    @Transactional //트렌젝션 메서드
-    public Article update(long id, UpdateArticleRequest request){
-        Article article = blogRepository.findById(id).orElseThrow(()->new IllegalArgumentException("not found: " + id));
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
 
-        article.update(request.getTitle(),request.getContent());
+        authorizeArticleAuthor(article);
+        article.update(request.getTitle(), request.getContent());
 
         return article;
     }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
+
 }
