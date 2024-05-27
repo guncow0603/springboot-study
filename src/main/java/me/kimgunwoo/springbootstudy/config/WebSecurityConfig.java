cb0f0609c2324ed66224d7858183adb1acab79b1
kimgunwoo <guncow0603@gmail.com>
2024-05-27 15:26:03 +0900

    csrf 비활성화

--- src/main/java/me/kimgunwoo/springbootstudy/config/WebSecurityConfig.java
+++ src/main/java/me/kimgunwoo/springbootstudy/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig {
                 .logoutSuccessUrl("/login")
                 .invalidateHttpSession(true)
                 .and()
+                .csrf().disable()
                 .build();
     }
 
